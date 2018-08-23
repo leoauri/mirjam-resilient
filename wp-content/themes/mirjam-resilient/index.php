9c428e8be4498b2e271c17ea593c16d9166a0f9e
leoauri <code@leoauri.com>
2018-08-23 10:10:09 +0200

    Initialise Theme

---
+++ wp-content/themes/mirjam-resilient/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Mirjam Resilient
+ **/
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'mirjamresilient' ); ?></a>
+		<header id="masthead" class="site-header" role="banner">
+			<!-- Header picture -->
+			<!-- Site title -->
+			<div class="navigation-top">
+				<!-- Navigation menu -->
+			</div>
+		</header>
+		<main id="main" class="site-main" role="main">
+			<!-- Page title -->
+			<!-- Page content -->
+		</main>
+	</div><!-- #page -->
+</body></html>
