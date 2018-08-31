52267067f45c6da2230fe240823c13bd1f8cb9a9
leoauri <code@leoauri.com>
2018-08-31 13:37:02 +0200

    Add header.php and refactor

---
+++ wp-content/themes/mirjam-resilient/header.php
@@ -0,0 +1,16 @@
+<?php
+namespace Mirjamresilient;
+/**
+ * Mirjam Resilient
+ **/
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<div id="page" class="site">
--- wp-content/themes/mirjam-resilient/index.php
+++ wp-content/themes/mirjam-resilient/index.php
@@ -3,16 +3,9 @@ namespace Mirjamresilient;
 /**
  * Mirjam Resilient
  **/
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-	<div id="page" class="site">
+
+get_header(); ?>
 		<header id="masthead" class="site-header" role="banner">
 			<img src="<?php echo(get_stylesheet_directory_uri()); ?>/assets/image/rays-still.jpg" />
 			<h1>
